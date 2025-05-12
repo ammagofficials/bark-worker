329fafdb2c4a007470ff8faeb15e08a2c2b63a43
zohaibqadir <zohaib.qadir786@hotmail.com>
2025-05-12 23:01:09 +0500

    updated stuff

--- start.sh
+++ start.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -e
 
-echo "Installing requirements..."
+echo "Installing basic dependencies..."
 pip install -r requirements.txt
 
-echo "Installing soundfile manually..."
+echo "Installing soundfile..."
 pip install soundfile
 
 echo "Installing Bark from GitHub..."
 pip install git+https://github.com/suno-ai/bark.git
 
-echo "Starting main app..."
+echo "Starting app..."
 python rp_handler.py
