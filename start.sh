d88107fac9cd5f41866b9daf54d101ea0ada2488
zohaibqadir <zohaib.qadir786@hotmail.com>
2025-05-12 23:10:09 +0500

    updated

--- start.sh
+++ start.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 set -e
 
-echo "Installing basic dependencies..."
+echo "Installing CUDA-enabled PyTorch..."
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+
+echo "Installing basic Python requirements..."
 pip install -r requirements.txt
 
 echo "Installing soundfile..."
 pip install soundfile
 
+echo "Installing Transformers (latest from HuggingFace GitHub)..."
+pip install git+https://github.com/huggingface/transformers.git
+
 echo "Installing Bark from GitHub..."
 pip install git+https://github.com/suno-ai/bark.git
-
-echo "Installing transformers from GitHub..."
-pip install git+https://github.com/huggingface/transformers.git
 
 echo "Starting app..."
 python rp_handler.py
