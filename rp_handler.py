66b7200b3152d9798ee4be51af7c6af89a0c4d52
zohaibqadir <zohaib.qadir786@hotmail.com>
2025-05-12 21:17:35 +0500

    updated handler

--- rp_handler.py
+++ rp_handler.py
@@ -9,12 +9,11 @@ def handler(event):
     prompt = input.get('prompt')
 
     device = "cuda" if torch.cuda.is_available() else "cpu"
-    model = BarkModel.from_pretrained("suno/bark", torch_dtype=torch.float16).to(device)
-    model.enable_cpu_offload()
-    # model = model.to_bettertransformer()
-    print(device+ " used")
+    print(device)
     processor = AutoProcessor.from_pretrained("suno/bark")
-    voice_preset = "v2/en_speaker_2"
+    model = BarkModel.from_pretrained("suno/bark")
+
+    voice_preset = "v2/en_speaker_6"
 
     inputs = processor("Hello, my dog is cute", voice_preset=voice_preset)
 
@@ -22,6 +21,22 @@ def handler(event):
     audio_array = audio_array.cpu().numpy().squeeze()
     sample_rate = model.generation_config.sample_rate
     scipy.io.wavfile.write("bark_out.wav", rate=sample_rate, data=audio_array)
+
+    # device = "cuda" if torch.cuda.is_available() else "cpu"
+    # print(device + " used")
+    # model = BarkModel.from_pretrained("suno/bark")
+    # model.enable_cpu_offload()
+    # # model = model.to_bettertransformer()
+    #
+    # processor = AutoProcessor.from_pretrained("suno/bark")
+    # voice_preset = "v2/en_speaker_2"
+    #
+    # inputs = processor("Hello, my dog is cute", voice_preset=voice_preset)
+    #
+    # audio_array = model.generate(**inputs)
+    # audio_array = audio_array.cpu().numpy().squeeze()
+    # sample_rate = model.generation_config.sample_rate
+    # scipy.io.wavfile.write("bark_out.wav", rate=sample_rate, data=audio_array)
     return prompt 
 
 if __name__ == '__main__':
