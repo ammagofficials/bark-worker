852fbb8a88432e02c74600c22c27c77ca6e0e01c
zohaibqadir <zohaib.qadir786@hotmail.com>
2025-05-12 23:07:31 +0500

    updated

--- start.sh
+++ start.sh
@@ -10,5 +10,8 @@ pip install soundfile
 echo "Installing Bark from GitHub..."
 pip install git+https://github.com/suno-ai/bark.git
 
+echo "Installing transformers from GitHub..."
+pip install git+https://github.com/huggingface/transformers.git
+
 echo "Starting app..."
 python rp_handler.py
