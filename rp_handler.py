edd09bdfb306b64e7f26f5f8fb630c6c19f16290
zohaibqadir <zohaib.qadir786@hotmail.com>
2025-05-12 21:45:23 +0500

    updated handler worker

--- rp_handler.py
+++ rp_handler.py
@@ -1,43 +1,51 @@
 import runpod
 from transformers import AutoProcessor, BarkModel
-import scipy
+import scipy.io.wavfile
 import torch
+import io
+import base64
+
+
 def handler(event):
-    print(f"Worker Start")
+    print("Worker Start")
     input = event['input']
-    
-    prompt = input.get('prompt')
+
+    prompt = input.get('prompt', "Hey, I am an awesome AI")
+    voice_preset = input.get("speaker", "v2/en_speaker_6")
 
     device = "cuda" if torch.cuda.is_available() else "cpu"
-    print(device)
+    print(f"Using device: {device}")
+
+    # Load processor and model
     processor = AutoProcessor.from_pretrained("suno/bark")
-    model = BarkModel.from_pretrained("suno/bark")
+    model = BarkModel.from_pretrained("suno/bark", torch_dtype=torch.float32).to(device)
 
-    voice_preset = "v2/en_speaker_6"
+    # Prepare inputs and move to correct device
+    inputs = processor(prompt, voice_preset=voice_preset, return_tensors="pt")
+    inputs = {key: value.to(device) for key, value in inputs.items()}
 
-    inputs = processor("Hello, my dog is cute", voice_preset=voice_preset)
-
+    # Generate audio
     audio_array = model.generate(**inputs)
     audio_array = audio_array.cpu().numpy().squeeze()
     sample_rate = model.generation_config.sample_rate
-    scipy.io.wavfile.write("bark_out.wav", rate=sample_rate, data=audio_array)
 
-    # device = "cuda" if torch.cuda.is_available() else "cpu"
-    # print(device + " used")
-    # model = BarkModel.from_pretrained("suno/bark")
-    # model.enable_cpu_offload()
-    # # model = model.to_bettertransformer()
-    #
-    # processor = AutoProcessor.from_pretrained("suno/bark")
-    # voice_preset = "v2/en_speaker_2"
-    #
-    # inputs = processor("Hello, my dog is cute", voice_preset=voice_preset)
-    #
-    # audio_array = model.generate(**inputs)
-    # audio_array = audio_array.cpu().numpy().squeeze()
-    # sample_rate = model.generation_config.sample_rate
-    # scipy.io.wavfile.write("bark_out.wav", rate=sample_rate, data=audio_array)
-    return prompt 
+    # Write audio to memory buffer instead of saving to file
+    buffer = io.BytesIO()
+    scipy.io.wavfile.write(buffer, rate=sample_rate, data=audio_array)
+    buffer.seek(0)
+
+    # Encode buffer content to base64
+    audio_base64 = base64.b64encode(buffer.read()).decode('utf-8')
+
+    return {
+        "status": "success",
+        "text": prompt,
+        "audio_base64": audio_base64,
+        "sample_rate": sample_rate,
+        "format": "wav",
+        "device":device
+    }
+
 
 if __name__ == '__main__':
-    runpod.serverless.start({'handler': handler })
+    runpod.serverless.start({'handler': handler})
